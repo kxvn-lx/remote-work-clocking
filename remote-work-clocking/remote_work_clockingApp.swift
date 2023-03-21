ca26b4ca724c87e00abecf43030ea0da31f282b5
Kevin Laminto <kevin.laminto@gmail.com>
2023-03-21 15:13:24 +1100

    Initial Commit

---
+++ remote-work-clocking/remote_work_clockingApp.swift
@@ -0,0 +1,17 @@
+//
+//  remote_work_clockingApp.swift
+//  remote-work-clocking
+//
+//  Created by Kevin Laminto on 21/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct remote_work_clockingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
